0ad815bc3cdb13aa4d5cc79d4d96e17639f216d6
Marceli Borowczak <marcelborowczak@outlook.com>
2024-09-13 16:06:04 +0200

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <SpeedInsights />
+        <Analytics />
         </body>
     </html>
   )
